d0e350bb9e9babe4994f2582d08ec841fa33f9ab
florianpfluecker <73837515+florianpfluecker@users.noreply.github.com>
2021-07-05 21:10:12 +0200

    layer 2 assets + scaling in test.js

--- test.js
+++ test.js
@@ -19,8 +19,14 @@ let images = {
   coral2: loadImage("./assets/layer1/coral2.png"),
   switchLayer: loadImage("./assets/layer1/switchLayer.png"),
   //Layer 2 dead Corals
-  coral3:
-
+  coral3: loadImage("./assets/layer2/coral3.png"),
+  coral4: loadImage("./assets/layer2/coral4.png"),
+  coral5: loadImage("./assets/layer2/coral5.png"),
+  coral6: loadImage("./assets/layer2/coral6.png"),
+  coral7: loadImage("./assets/layer2/coral7.png"),
+  coral8: loadImage("./assets/layer2/coral8.png"),
+  coral9: loadImage("./assets/layer2/coral9.png"),
+  switchLayer2: loadImage("./assets/layer2/switchLayer2.png"),
 };
 
 // import Decision from "./classes/Decision";
@@ -41,6 +47,14 @@ let images = {
 function draw() {
   // image(images.layer1, 0, 0, 1920, 1080);
   image(images.layer2, 0, 0, 1920, 1080);
+  image(images.coral3, 252, 230, 119 * 1.4, 49 * 1.4);
+  image(images.coral4, 110, 635, 297 * 1.4, 189 * 1.4);
+  image(images.coral5, 670, 470, 84 * 1.4, 75 * 1.4);
+  image(images.coral6, 902, 335, 92 * 1.4, 70 * 1.4);
+  image(images.coral7, 1203, 514, 224 * 1.4, 100 * 1.4);
+  image(images.coral8, 1210, 0, 222 * 1.4, 173 * 1.4);
+  image(images.coral9, 782, 94, 36 * 1.4, 28 * 1.4);
+  image(images.switchLayer2, 1135, 18, 52 * 1.4, 25 * 1.4);
 
   // image(images.coral1, 1340, 925, 118 * 1.4, 100 * 1.4);
   // image(images.coral2, 1668, 595, 142.5, 150);
