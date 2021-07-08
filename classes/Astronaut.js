92929cb125fe0d73723502d031c38def8093ebd1
florianpfluecker <73837515+florianpfluecker@users.noreply.github.com>
2021-07-08 11:26:39 +0200

    scale Astronaut+Robo class

--- classes/Astronaut.js
+++ classes/Astronaut.js
@@ -2,12 +2,13 @@ export default class Astronaut {
   constructor(x, y, images, scale) {
     this.x = x;
     this.y = y;
-    this.scale = scale;
     this.astronaut = images.astronaut;
+    this.scale = scale;
   }
 
   drawAstronaut() {
-    image(this.astronaut, this.x, this.y, 306, 396);
+    this.scale = scale;
+    image(this.astronaut, this.x, this.y, 306 * this.scale, 396 * this.scale);
   }
 
   display() {
